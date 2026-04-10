8371aa714705192d2812616741b905ed4d5ee3fc
talaganaRajesh <talaganarajesh@gmail.com>
2026-04-10 22:54:20 +0530

    gap in journal section homepage

--- app/home/CoopImages.jsx
+++ app/home/CoopImages.jsx
@@ -57,7 +57,7 @@ export default function CoopImages({ locations = [] }) {
   const STEVENAGE_IMG = stevenage?.image?.asset?.url || null;
 
   return (
-    <div className="flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap gap-[8vw] md:gap-[2vw] lg:gap-[2.5vw] xl:gap-[1.5vw] w-full justify-center lg:justify-between items-stretch px-[5vw] md:px-[1.4vw] lg:px-[2.5vw] xl:px-[1.3vw] pt-[1vw] pb-[10vw] md:pb-[6vw] lg:py-[7vw] xl:py-[3.5vw]">
+    <div className="flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap gap-[8vw] md:gap-[2vw] lg:gap-[2.5vw] xl:gap-[1.5vw] w-full justify-center lg:justify-between items-stretch px-[5vw] md:px-[1.4vw] lg:px-[2.5vw] xl:px-[1.3vw] pt-[1vw] pb-[4vw] md:pb-[6vw] lg:py-[7vw] xl:py-[3.5vw]">
       {/* Hitchin Mobile & Tablet */}
       <Link
         href="/hitchin"
