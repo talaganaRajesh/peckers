2b5dbd1f1698b37d27221f9c7bb808f4e00bcf01
Neelapu Nani <neelapunani2580@gmail.com>
2026-04-14 08:38:35 +0530

    Decrese the size of that Menu in Mobile view

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -245,7 +245,7 @@ export default function Navbar({ preloadedSettings = null }) {
       <div
         className={`absolute top-full -mt-px left-0 right-0 w-full h-auto max-h-[85vh] bg-black flex flex-col transition-all duration-500 ease-in-out md:hidden z-40 overflow-y-auto overflow-x-hidden scrollbar-hide shadow-2xl rounded-b-[2.5rem] ${open ? "opacity-100 translate-y-0 pointer-events-auto" : "opacity-0 -translate-y-4 pointer-events-none"}`}
       >
-        <div className="flex flex-col px-[8vw] py-[8vw] space-y-[1vw] relative min-h-[70vh]">
+        <div className="flex flex-col px-[8vw] py-[4vw] space-y-[1vw] relative min-h-[70vh]">
           {/* Decorative Background Text - Zinc Color */}
           <div className="absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10 opacity-[0.05] select-none pointer-events-none whitespace-nowrap">
             <h2 className="text-[40vw] font-black italic transform -rotate-12 text-zinc-700">PECKERS</h2>
